693e5505d4587c63fc6ed53b7347c219ff099540
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-08-25 12:37:30 -0300

    finished extract function

--- src/extract.py
+++ src/extract.py
@@ -16,7 +16,3 @@ def extract_data(file_path):
     except Exception as e:
         print(f"Error extracting data from {file_path}: {e}")
         return None
-
-if __name__ == "__main__":
-    file_path = './input_data/sales_data.csv'
-    raw_df = extract_data(file_path)
