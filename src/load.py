f95af5271aec867b24ca58ab2c5e51d4ca5f1a38
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-08-26 23:29:14 -0300

    maganed to upload data from a dataframe to snowflake

--- src/load.py
+++ src/load.py
@@ -20,25 +20,23 @@ def load_data(df):
     """
     try:
         # Establish Snowflake connection
-        #df.reset_index(drop=True, inplace=True)
-        dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
-        load_dotenv(dotenv_path)
+        load_dotenv('/home/gabriel/repos/retail-snowflake-etl/.env')
         conn = snowflake.connector.connect(
             user=os.getenv('SF_USER'),
             password=os.getenv('SF_PASSWORD'),
             account=os.getenv('SF_ACCOUNT'),
             warehouse=os.getenv('SF_WAREHOUSE'),
             database=os.getenv('SF_DATABASE'),
-            schema=os.getenv('SF_SCHEMA')
+            schema='STG'
         )
         
         # Load data into Snowflake
         success, nchunks, nrows, _ = write_pandas(
             conn=conn,
             df=df,
-            table_name='FACT_SALES',
+            table_name='RAW_SALES',
             database=os.getenv('SF_DATABASE'),
-            schema=os.getenv('SF_SCHEMA')
+            schema='STG'
         )
         conn.close()
         
@@ -53,22 +51,7 @@ def load_data(df):
         return False
 
 if __name__ == "__main__":
-    #file_path = './input_data/sales_data.csv'
-    #raw_df = extract_data(file_path)
-    #cleaned_df = transform_data(raw_df)
-    data = {
-    "SALE_ID": [10, 20, 30],
-    "DATE_ID": ["2025-08-01", "2025-08-02", "2025-08-03"],
-    "PRODUCT_ID": [101, 102, 103],
-    "REGION_ID": [1, 2, 3],
-    "SALES_REP_ID": [11, 12, 13],
-    "QUANTITY_SOLD": [5, 3, 8],
-    "AMOUNT_SOLD": [500.0, 300.0, 800.0],
-    "DISCOUNT": [50.0, 0.0, 80.0],
-    "PAYMENT_METHOD": ["CREDIT_CARD", "CASH", "TRANSFER"],
-    "SALES_CHANNEL": ["ONLINE", "STORE", "ONLINE"]
-    }
-    df_test = pd.DataFrame(data)
-    df_test.columns = df_test.columns.str.upper()
-    df_test["DATE_ID"] = df_test['DATE_ID'] = pd.to_datetime(df_test['DATE_ID']).dt.date
-    load_data(df_test)
+    file_path = '../input_data/sales_data.csv'
+    raw_df = extract_data(file_path)
+    cleaned_df = transform_data(raw_df)
+    load_data(cleaned_df)
